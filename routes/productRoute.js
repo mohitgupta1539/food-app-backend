3b217529ac96a617f202eb8734ac42c8798f3273
MOHIT GUPTA <mohitgupta17042000@gmail.com>
2023-05-07 17:22:53 +0530

    git updated

---
+++ routes/productRoute.js
@@ -0,0 +1,78 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+    braintreePaymentController,
+    braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  filterProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productListController,
+  productPhotoController,
+  relatedProductController,
+  searchProductController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", filterProductController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//product search
+router.get('/search/:keyword', searchProductController);
+
+//similar product
+router.get('/related-product/:pid/:cid', relatedProductController);
+
+//category wise product
+router.get('/product-category/:slug', productCategoryController);
+
+//payment routes
+//token
+router.get('/braintree/token', braintreeTokenController);
+
+//payment
+router.post('/braintree/payment', requireSignIn, braintreePaymentController);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// const express = require('express');
+// const dotenv = require('dotenv');
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoute.js';
+import productRoutes from './routes/productRoute.js';
+import cors from 'cors';
+import path from "path";
+
+
+//configure env
+dotenv.config()
+
+//connection mongodb
+connectDB();
+
+//rest  object
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "./client/build")))
+
+//routes
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/category', categoryRoutes)
+app.use('/api/v1/product', productRoutes)
+
+//rest api
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+// app.get('/', (req, res) => {
+//     res.send({
+//         message:'ecommerce app started'
+//     })
+// })
+
+
+//PORT
+// const PORT = 8080
+const PORT = process.env.PORT || 8080
+
+
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`server Running on ${PORT}`)
+});
